955f6b521f3ac29a7f2fc4522c3d9efe58cc6c7d
JavierIQ <javier.iniguez2498@gmail.com>
2023-10-31 18:44:48 -0600

    SampleStructure

---
+++ app/src/main/java/com/javieriq/pokeapicompose/PokeApiComposeApplication.kt
@@ -0,0 +1,14 @@
+package com.javieriq.pokeapicompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PokeApiComposeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
